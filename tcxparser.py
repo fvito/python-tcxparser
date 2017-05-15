f25f18332ebeba4aa76335993471afddb57e4731
Vito Ferš <fvito@users.noreply.github.com>
2017-05-15 15:10:21 +0200

    Added values for speed(min, max, avg)

--- tcxparser.py
+++ tcxparser.py
@@ -1,26 +1,31 @@
 "Simple parser for Garmin TCX files."
 
 import time
+
 from lxml import objectify
 
 namespace = 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2'
+ext_namespace = 'http://www.garmin.com/xmlschemas/ActivityExtension/v2'
 
 
-class TCXParser:
-
+class TCXParser(object):
     def __init__(self, tcx_file):
         tree = objectify.parse(tcx_file)
         self.root = tree.getroot()
         self.activity = self.root.Activities.Activity
 
     def hr_values(self):
-        return [int(x.text) for x in self.root.xpath('//ns:HeartRateBpm/ns:Value', namespaces={'ns': namespace})]
+        return [x.pyval for x in self.root.xpath('//ns:HeartRateBpm/ns:Value', namespaces={'ns': namespace})]
 
     def altitude_points(self):
-        return [float(x.text) for x in self.root.xpath('//ns:AltitudeMeters', namespaces={'ns': namespace})]
+        return [x.pyval for x in self.root.xpath('//ns:AltitudeMeters', namespaces={'ns': namespace})]
 
     def time_values(self):
         return [x.text for x in self.root.xpath('//ns:Time', namespaces={'ns': namespace})]
+
+    def speed_values(self):
+        return [x.pyval for x in self.root.xpath('//ns:Trackpoint//ns:Extensions//ns2:TPX//ns2:Speed',
+                                                 namespaces={'ns': namespace, 'ns2': ext_namespace})]
 
     @property
     def latitude(self):
@@ -62,12 +67,12 @@ class TCXParser:
 
     @property
     def hr_avg(self):
-        """Average heart rate of the workout"""
+        """Average heart rate of the workout, 0 if unable to calculate"""
         hr_data = self.hr_values()
         if len(hr_data) < 1:
             return 0
         else:
-            return sum(hr_data)/len(hr_data)
+            return sum(hr_data) / len(hr_data)
 
     @property
     def hr_max(self):
@@ -82,17 +87,40 @@ class TCXParser:
     @property
     def pace(self):
         """Average pace (mm:ss/km for the workout"""
-        secs_per_km = self.duration/(self.distance/1000)
+        secs_per_km = self.duration / (self.distance / 1000)
         return time.strftime('%M:%S', time.gmtime(secs_per_km))
 
     @property
+    def speed_min(self):
+        """Minimum speed of the workout"""
+        return min(self.speed_values())
+
+    @property
+    def speed_max(self):
+        """Maximum speed of the workout"""
+        return max(self.speed_values())
+
+    @property
+    def speed_avg(self):
+        """Average speed of the workout, 0 if unable to calculate"""
+        speed_data = self.speed_values()
+        if len(speed_data) < 1:
+            return 0
+        else:
+            return sum(speed_data) / len(speed_data)
+
+    @property
+    def speed_units(self):
+        return 'm/s'
+
+    @property
     def altitude_avg(self):
-        """Average altitude for the workout"""
+        """Average altitude for the workout, 0 if unable to calculate"""
         altitude_data = self.altitude_points()
         if len(altitude_data) < 1:
             return 0
         else:
-            return sum(altitude_data)/len(altitude_data)
+            return sum(altitude_data) / len(altitude_data)
 
     @property
     def altitude_max(self):
@@ -112,7 +140,7 @@ class TCXParser:
         total_ascent = 0.0
         altitude_data = self.altitude_points()
         for i in range(len(altitude_data) - 1):
-            diff = altitude_data[i+1] - altitude_data[i]
+            diff = altitude_data[i + 1] - altitude_data[i]
             if diff > 0.0:
                 total_ascent += diff
         return total_ascent
@@ -123,7 +151,29 @@ class TCXParser:
         total_descent = 0.0
         altitude_data = self.altitude_points()
         for i in range(len(altitude_data) - 1):
-            diff = altitude_data[i+1] - altitude_data[i]
+            diff = altitude_data[i + 1] - altitude_data[i]
             if diff < 0.0:
                 total_descent += abs(diff)
         return total_descent
+
+
+class Trackpoint(object):
+    def __init__(self, lat, lng):
+        self.latitude = lat
+        self.longitude = lng
+
+    @property
+    def latitude(self):
+        return self.latitude
+
+    @latitude.setter
+    def latitude(self, value):
+        self.latitude = value
+
+    @property
+    def longitude(self):
+        return self.longitude
+
+    @longitude.setter
+    def longitude(self, value):
+        self.longitude = value
